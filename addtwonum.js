1fe973742e4e09cd9f51ef99ae584e61d5091e0b
kalai7788 <skalaiyarasu0@gmail.com>
2025-06-04 18:03:11 +0530

    Create addtwonum.js

---
+++ addtwonum.js
@@ -0,0 +1,7 @@
+// get two integer number, add them and print it on console
+
+const a = parseInt(promt("Enter your first number");
+const b = parseInt(promt("Enter your second  number");
+
+const c = a+b;
+console.log("total: "+ c);
