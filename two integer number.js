b26b033495738fa485982113258fea80f658e67a
kalai7788 <skalaiyarasu0@gmail.com>
2025-06-04 18:49:48 +0530

    two integer number.js

---
+++ two integer number.js
@@ -0,0 +1,5 @@
+// get two integer number, division them and print it on console
+let a = parseInt(prompt("Enter first number: "));
+let b = parseInt(prompt("Enter second number: "));
+let c = a/b;
+console.log("Division result: " + c);
