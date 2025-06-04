5be44346c537afa01ef670660478ab0ccffbc9d6
kalai7788 <skalaiyarasu0@gmail.com>
2025-06-04 18:47:40 +0530

    two decimal number.js

---
+++ two decimal number.js
@@ -0,0 +1,6 @@
+// get two decimal number, division them and print it on console
+let a = parseFloat(prompt("Enter first number: "));
+let b = parseFloat(prompt("Enter second number: "));
+let c = a/b;
+let  = c.toFixted(2);
+console.log("Division result: " + c);
