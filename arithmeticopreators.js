ea471b726418778dc0065d10412c80ce585dcd1a
kalai7788 <skalaiyarasu0@gmail.com>
2025-06-04 18:32:44 +0530

    arithmeticopreators.js

---
+++ arithmeticopreators.js
@@ -0,0 +1,12 @@
+// write a program that uses of arithmetic opreators
+ let a =  parseInt(promt("Enter your frist number: "));
+ let b =  parseInt(promt("Enter your frist number: "));
+let c = a+=b;
+let d = a-=b;
+let e = a*=b;
+let f = a/=b;
+
+console.log("total add: "+c);
+console.log("total add: "+d);
+console.log("total add: "+e);
+console.log("total add: "+f);
