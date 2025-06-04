46b9487dd619cacf4c5ccc3d2c855b6f3720c32d
kalai7788 <skalaiyarasu0@gmail.com>
2025-06-04 18:11:18 +0530

    Create arithmeticopertors.js

---
+++ arithmeticopertors.js
@@ -0,0 +1,14 @@
+// write a program that uses of arithmetic opertors
+ let a =  parseInt(promt("Enter your frist number: "));
+ let b =  parseInt(promt("Enter your frist number: "));
+let c = a+b;
+let d = a-b;
+let e = a*b;
+let f = a/b;
+
+console.log("total add: "+c);
+console.log("total add: "+d);
+console.log("total add: "+e);
+console.log("total add: "+f);
+
+
