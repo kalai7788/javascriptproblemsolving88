01634ae2802b160472544eebdde4016c77e46205
kalaiyarasu <skalaiyarasu0@gmail.com>
2025-06-05 16:11:39 +0530

    two long float.js

---
+++ two long float.js
@@ -0,0 +1,7 @@
+// Define a two long float number and print it on console
+
+let a = 19.253435;
+let b = 20.365744;
+
+console.log("This is your first value" + a);
+console.log("This is your seacond value" + b);
