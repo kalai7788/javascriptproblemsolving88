1fdab503380df4053e41b5f03b1eb13f0ba94634
kalaiyarasu <skalaiyarasu0@gmail.com>
2025-06-06 12:42:58 +0530

    Create less than number.js

---
+++ less than number.js
@@ -0,0 +1,11 @@
+//check the number greate than 100 and less then 50
+
+const number = parseFloat(prompt("Enter a number: "));
+
+if (number > 100) {
+  console.log(" The number is greater than 100.");
+} else if (number < 50) {
+  console.log(" The number is less than 50.");
+} else {
+  console.log(" The number is between 50 and 100.");
+}
