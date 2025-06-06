8efaf0ead0d5a34488f71b70e27b4a2c06ba49db
kalaiyarasu <skalaiyarasu0@gmail.com>
2025-06-06 12:36:29 +0530

    Create char.js

---
+++ char.js
@@ -0,0 +1,10 @@
+//get inuput for user wholenumber,decimalnumber,char
+
+const wholeNumber = parseInt(prompt("Enter an integer number: "));
+const decimalNumber = parseFloat(prompt("Enter a decimal number: "));
+const character = prompt("Enter a single character: ");
+
+console.log(" Here is your input:");
+console.log("Integer Number: ", wholeNumber);
+console.log("Decimal Number:", decimalNumber);
+console.log("Character:      ", character);
