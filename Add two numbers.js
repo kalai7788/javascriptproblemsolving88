dcd1ccb625b43ad9a0f70e474de5eead093ad47c
kalaiyarasu <skalaiyarasu0@gmail.com>
2025-06-06 11:48:54 +0530

    Add two numbers.js

---
+++ Add two numbers.js
@@ -0,0 +1,7 @@
+// get a number from user and add them and print it on console
+
+const a = parseInt(prompt("Enter the value: ");
+const b = parseInt(prompt("Enter the value: ");
+
+let c = a+b;
+console.log("Result of = "+c);
