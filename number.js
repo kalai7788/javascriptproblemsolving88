b7cc0880f752c1087be165e863bc6b76ab1a902d
kalaiyarasu <skalaiyarasu0@gmail.com>
2025-06-06 11:45:51 +0530

    number.js

---
+++ number.js
@@ -0,0 +1,9 @@
+////get a number from user and the number those many times and add them and print it on console
+
+const number = parseInt(prompt("Enter the value: ");
+let sum = 0;
+
+for(let i = 0; i <= number; i++){
+sum += number;
+}
+console.log("The  sum of total: "+sum);
